68c8ec48f7164b8540f0d44727ab01c0fb975a0a
Emily Hodges <ehodges9@gatech.edu>
2020-11-28 17:17:49 -0500

    Created and formatted About page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -2,9 +2,30 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About us page</h1>
+        <>
+        <img class="aboutImg" src="images/about.jpg" />
+        <div class="aboutPage">
+            <p class="heading">Our mission</p>
+            <p>Our mission is to advocate for animals by providing safety until home placement
+                of those in our care. We promote spay and neuter programs as a means to control pet
+                overpopulation.
+            </p>
+            <p class="heading">Wish list</p>
+            <p>We always have a long shopping list at the Shelter! Donated items can be dropped off at the 
+                shelter located at 56 Broad Street in Quincy during adoption hours and left with any of
+                our Volunteers or shipped directly to us from Amazon. Check out our wish list!
+            </p>
+            <p class="heading">Volunteer</p>
+            <p>Volunteers are the backbone of our efforts to improve the lives of the animals in our care. You 
+                can also become a guardian angel and sponsor a cage or kennel at the shelter. You can become a 
+                member of QAS or help out with one of our available positions at QAS.
+            </p>
+            <p class="heading">Adoption hours</p>
+            <p><b>Tuesday - </b>6-9 PM</p>
+            <p><b>Thursday - </b>6-8 PM</p>
+            <p><b>Saturday - </b>10 AM - 4 PM</p>
         </div>
+        </>
     );
 }
 
