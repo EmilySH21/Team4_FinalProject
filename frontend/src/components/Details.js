dedc5d1f0482cfc64571537d4ff4330e62fa8607
Fareeda Kasim <kasimfareeda@gmail.com>
2020-11-29 00:37:03 -0500

    styling changes

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -5,18 +5,21 @@ const Details = (data) => {
     return (
         <div class="petDetails">
             <h1 class="petTitle">{data.name}</h1>
+            <div>
+                <img src={data.imgSrc} alt='image of pet'></img>
+            </div>
             <div class="petSummary">
-                <img class="icon" src="public/images/animal_icon.jpg" />
+                <img class="icon" src="images/animal_icon.jpg" />
                 {data.breed}
             </div>
             <br></br>
             <div class="petSummary">
-                <img class="icon" src="public/images/needle_icon.jpg" />
+                <img class="icon" src="images/needle_icon.jpg" />
                 {data.status}
             </div> 
             <br></br>
             <div class="petSummary">
-                <img class="icon" src="public/images/info_icon.jpg" />
+                <img class="icon" src="images/info_icon.jpg" />
                 {data.gender + ", " + data.age}
             </div>
             <div class="petParagraph">
